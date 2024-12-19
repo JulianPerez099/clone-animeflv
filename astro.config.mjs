9075416caacf9d0afd2266af66a0406d78d929b4
Julian David Perez <jdgp90998@gmail.com>
2024-12-18 20:48:39 -0500

    Add Cloudflare adapter and dependency to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 
-import vercel from '@astrojs/vercel/serverless';
-
 import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: cloudflare(),
   integrations: [tailwind(), react()]
 });
